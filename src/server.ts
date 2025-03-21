398814b5f4e51d7d5c8c2aedad5ad4fef63a686d
Lozita <lozanoruizj@gmail.com>
2025-03-21 12:25:07 +0100

    0.05

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,37 @@
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit'; 
+import verificarToken from './middleware/auth.middleware.js'; 
 import profesoresRoutes from './routes/profesores.routes.js';
-import verificarToken from './middlewares/auth.middleware.js';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
+const angularApp = new AngularNodeAppEngine();
 
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 15, 
+  message: 'Demasiadas solicitudes desde esta IP, inténtalo de nuevo más tarde.',
+});
 
 app.use(cors({ origin: 'http://localhost:4200' })); 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+app.use(limiter); 
 
-// Rutas
+
 app.use('/api/profesores', verificarToken, profesoresRoutes);
 
-// Servir archivos estáticos de Angular
 app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
@@ -26,7 +40,6 @@ app.use(
   }),
 );
 
-// Render Angular Universal
 app.use('/**', (req, res, next) => {
   angularApp
     .handle(req)
@@ -36,10 +49,12 @@ app.use('/**', (req, res, next) => {
     .catch(next);
 });
 
-// Inicia el servidor
-const port = process.env['PORT'] || 4000;
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`);
-});
+// Iniciar el servidor
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, () => {
+    console.log(`Servidor Node escuchando en http://localhost:${port}`);
+  });
+}
 
 export const reqHandler = createNodeRequestHandler(app);
