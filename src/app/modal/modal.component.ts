7a59eb2d49dd2b615a64caa0e6b0b56ec10eb403
LozitaL <lozanoruizj@gmail.com>
2025-03-26 14:15:07 +0100

    0

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BodyComponent } from "../body/body.component"; 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [CommonModule, BodyComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
   standalone: true,
 })
 export class ModalComponent {
+  currentView: string = ''; 
 
+  changeView(view: string): void {
+    this.currentView = view;
+  }
 }
